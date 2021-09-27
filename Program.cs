dbfdbbf3a8571eafabf559b9dc83498f4a4849ca
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:44:03 -0300

    Adiciona atividade IMC

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Calcule o índice de massa corporal de uma pessoa, dados altura (em m) e peso (em kg).
+
+            Console.WriteLine("Calculadora IMC\n");
+
+            Console.Write("Digite sua altura: ");
+            float m = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu peso: ");
+            float kg = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nIMC: {kg/Math.Pow(m,2)} kg/m²");
+        }
+    }
+}
